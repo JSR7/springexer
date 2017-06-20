ae46729f829d251ee183ede9d8258c11d1f2d9cf
JSR7 <G!thUb1!>
2017-06-20 14:39:08 -0500

    created SERVICE   (Service exercise)

---
+++ src/main/java/com/codeup/svcs/PostSvc.java
@@ -0,0 +1,45 @@
+package com.codeup.svcs;
+
+import com.codeup.models.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jsr on 6/20/17.
+ */
+
+
+@Service ("postSvc")
+public class PostSvc {
+    private List<Post> posts;
+
+    public PostSvc(){
+        createPosts();
+    }
+
+    public List<Post> findAll() {
+        return posts;
+    }
+
+    public Post save(Post post) {
+        post.setId(posts.size() + 1);
+        posts.add(post);
+        return post;
+    }
+
+    public Post findOne(long id) {
+        return posts.get((int) (id - 1));
+    }
+    private void createPosts(){
+        posts = new ArrayList<>();
+
+        save(new Post("Football for sale", "Gently used, $15"));
+        save(new Post("PS4 for sale", "great shape, must have"));
+
+    }
+
+
+
+}
